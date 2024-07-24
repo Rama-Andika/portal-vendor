7c482ea9b5d35de8bf552232215870db03bb1f23
I Putu Rama Andika Putra <70218936+Rama-Andika@users.noreply.github.com>
2024-07-24 15:14:20 +0800

    update portal vendor

--- src/components/functions/AccountingNumber.js
+++ src/components/functions/AccountingNumber.js
@@ -1,8 +1,19 @@
-const accountingNumber = (x) => {
-    var parts = x.toString().split(".");
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const accountingNumber = (number) => {
+    // var parts = x.toString().split(".");
+    // parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-    return parts.join(",");
+    // return parts.join(",");
+
+    if(isNaN(number)){
+      number = 0
+    }
+    const formattedNumber = new Intl.NumberFormat('en-US', {
+      style: 'decimal', // Style can be 'decimal', 'currency', 'percent'
+      minimumFractionDigits: 2, // Minimum number of decimal places
+      maximumFractionDigits: 2, // Maximum number of decimal places
+    }).format(number);
+  
+    return formattedNumber;
   }
 
   export default accountingNumber
